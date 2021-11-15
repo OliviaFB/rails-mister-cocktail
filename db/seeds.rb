e0f23689a9b64423f49bff11f242e170b799b093
Olivia Forbault <olivia.forbault@outlook.fr>
2021-11-15 12:08:40 +0100

    added and parsed new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
-Ingredient.create(name: "basil leaves")
-Ingredient.create(name: "honey")
-Ingredient.create(name: "olives")
-Ingredient.create(name: "gin")
-Ingredient.create(name: "sirop")
+require 'json'
+require 'open-uri'
 
-Cocktail.create(name: "Mojito")
-Cocktail.create(name: "Gin&Tonic")
-Cocktail.create(name: "Moscow Mule")
-Cocktail.create(name: "London Mule")
-Cocktail.create(name: "Pinacolada")
-Cocktail.create(name: "Long Island Iced Tea")
+Cocktail.create(name: "Long Island Ice Tea")
+Cocktail.create(name: "Margarita")
+Cocktail.create(name: "Cosmopolitan")
+Cocktail.create(name: "Vodka Martini")
+Cocktail.create(name: "Pina Colada")
+Cocktail.create(name: "Gin Fizz")
+Cocktail.create(name: "Old Fashioned")
+
+
+Ingredient.destroy_all
+
+result = JSON.parse(open('http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)
+drinks = result['drinks']
+
+drinks.each do |hash|
+  hash.each do |k, v|
+    Ingredient.create(name: v)
+  end
+end
